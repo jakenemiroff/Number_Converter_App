41a146c517b8e82a46d313943f97939662a648ce
Jake Nemiroff <jakenemiroff@gmail.com>
2020-02-14 18:58:56 -0500

    add functionality to convert integer to binary

--- app/src/main/java/com/jetbrains/handson/mpp/numberconverter/FirstFragment.kt
+++ app/src/main/java/com/jetbrains/handson/mpp/numberconverter/FirstFragment.kt
@@ -6,7 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import androidx.navigation.fragment.findNavController
+import android.widget.EditText
+import android.widget.TextView
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
@@ -27,5 +28,33 @@ class FirstFragment : Fragment() {
 //        view.findViewById<Button>(R.id.button_first).setOnClickListener {
 //            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
 //        }
+
+
+        view.findViewById<Button>(R.id.Encode).setOnClickListener {
+
+            encode(view)
+        }
     }
+}
+
+//function to increment the counted value when clicking on the `count` button
+private fun encode(view: View) {
+
+    // Get the EditText
+    val textToConvert = view.findViewById<EditText>(R.id.textToConvert)
+
+    // Get the text view
+    val displayAnswer = view.findViewById<TextView>(R.id.Answer)
+
+    // Get the value of the EditText.
+    val editTextValue = textToConvert.text.toString()
+
+    // Convert to Int
+    var numberToEncode: Int = Integer.parseInt(editTextValue)
+
+    // Convert integer to binary
+    var binaryNumber = Integer.toBinaryString(numberToEncode)
+
+    // Display the new value in the text view.
+    displayAnswer.text = binaryNumber
 }
